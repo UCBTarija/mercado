e0d9892c6c81db7046719f1ba8ded9ff30146a3b
Ronal Guerrero <ronal.guerrero@gmail.com>
2016-03-06 23:23:15 -0400

    Versión inicial

---
+++ 03.imp/mercado/mercado/Form1.cs
@@ -0,0 +1,28 @@
+﻿using mercado.dao;
+using mercado.model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mercado
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<Unidad> lsita = Unidad.getAll();
+
+        }
+    }
+}
---
+++ 03.imp/mercado/mercado/dao/UnidadDAO.cs
@@ -0,0 +1,32 @@
+﻿using mercado.model;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mercado.dao
+{
+    class UnidadDAO
+    {
+        public List<Unidad> getAll()
+        {
+            List<Unidad> lista = new List<Unidad>();
+            using (SqlConnection conn = DAOFactory.getConnection())
+            {
+                SqlCommand cmd = new SqlCommand("select id,nombre from unidad", conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+                Unidad u;
+                while (reader.Read())
+                {
+                    u = new Unidad();
+                    u.id = reader.GetString(0);
+                    u.nombre = reader.GetString(1);
+                    lista.Add(u);
+                }
+            }
+            return lista;
+        }
+    }
+}
